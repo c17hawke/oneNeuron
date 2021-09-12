c157b3dc63de6c898a54ed07cff6fce84acc3892
SUNNY BHAVEEN CHANDRA <2016peb5126@mnit.ac.in>
2021-09-12 18:51:02 +0530

    and file completed

--- and.py
+++ and.py
@@ -1,5 +1,5 @@
 from utils.model import Perceptron
-from utils.all_utils import prepare_data
+from utils.all_utils import prepare_data, save_plot, save_model
 import pandas as pd
 import numpy as np
 
@@ -12,7 +12,7 @@ AND = {
 
 df = pd.DataFrame(AND)
 
-df
+print(df)
 
 X,y = prepare_data(df)
 
@@ -23,3 +23,6 @@ model = Perceptron(eta=ETA, epochs=EPOCHS)
 model.fit(X, y)
 
 _ = model.total_loss()
+
+save_model(model, filename="and.model")
+save_plot(df, "and.png", model)
--- utils/model.py
+++ utils/model.py
@@ -1,5 +1,6 @@
 import numpy as np
 
+
 class Perceptron:
   def __init__(self, eta, epochs):
     self.weights = np.random.randn(3) * 1e-4 # SMALL WEIGHT INIT
