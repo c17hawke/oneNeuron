206aa97f85efa031c76677aec906949663770f8f
SUNNY BHAVEEN CHANDRA <2016peb5126@mnit.ac.in>
2021-09-13 19:40:49 +0530

    minor editing

--- and.py
+++ and.py
@@ -3,35 +3,22 @@ from utils.all_utils import prepare_data, save_plot, save_model
 import pandas as pd
 
 
-def main(data, modelname, plotname, eta, epochs):
-
+def main(data, modelName, plotName, eta, epochs):
     df = pd.DataFrame(data)
-
     print(df)
-
-    X,y = prepare_data(df)
-
-
-
+    X, y = prepare_data(df)
     model = Perceptron(eta=eta, epochs=epochs)
     model.fit(X, y)
-
     _ = model.total_loss()
-
-    save_model(model, filename=modelname)
-    save_plot(df, plotname, model)
-
-
+    save_model(model, filename=modelName)
+    save_plot(df, plotName, model)
 
 if __name__ == '__main__':
-    
     AND = {
         "x1": [0,0,1,1],
         "x2": [0,1,0,1],
         "y": [0,0,0,1],
     }
-
     ETA = 0.3 # 0 and 1
     EPOCHS = 10
-
-    main(data=AND, modelname="and.model", plotname="and.png", eta=ETA, epochs=EPOCHS)
+    main(data=AND, modelName="and.model", plotName="and.png", eta=ETA, epochs=EPOCHS)
--- utils/all_utils.py
+++ utils/all_utils.py
@@ -1,61 +1,58 @@
 import matplotlib.pyplot as plt
 import numpy as np
-import pandas as pd
-import joblib # FOR SAVING MY MODEL AS A BINARY FILE
+import joblib  # FOR SAVING MY MODEL AS A BINARY FILE
 from matplotlib.colors import ListedColormap
 import os
 
+
 plt.style.use("fivethirtyeight")
 
+
 def prepare_data(df):
-  X = df.drop("y", axis=1)
-  y = df["y"]
-  return X, y
+    x = df.drop("y", axis=1)
+    y = df["y"]
+    return x, y
+
 
 def save_model(model, filename):
-  model_dir = "models"
-  os.makedirs(model_dir, exist_ok=True) # ONLY CREATE IF MODEL_DIR DOESN"T EXISTS
-  filePath = os.path.join(model_dir, filename) # model/filename
-  joblib.dump(model, filePath)
+    model_dir = "models"
+    os.makedirs(model_dir, exist_ok=True)  # ONLY CREATE IF MODEL_DIR DOES NOT EXISTS
+    filepath = os.path.join(model_dir, filename)  # model/filename
+    joblib.dump(model, filepath)
+
 
 def save_plot(df, file_name, model):
-  def _create_base_plot(df):
-    df.plot(kind="scatter", x="x1", y="x2", c="y", s=100, cmap="winter")
-    plt.axhline(y=0, color="black", linestyle="--", linewidth=1)
-    plt.axvline(x=0, color="black", linestyle="--", linewidth=1)
-    figure = plt.gcf() # get current figure
-    figure.set_size_inches(10, 8)
+    def _create_base_plot(df):
+        df.plot(kind="scatter", x="x1", y="x2", c="y", s=100, cmap="winter")
+        plt.axhline(y=0, color="black", linestyle="--", linewidth=1)
+        plt.axvline(x=0, color="black", linestyle="--", linewidth=1)
+        figure = plt.gcf()  # get current figure
+        figure.set_size_inches(10, 8)
 
-  def _plot_decision_regions(X, y, classfier, resolution=0.02):
-    colors = ("red", "blue", "lightgreen", "gray", "cyan")
-    cmap = ListedColormap(colors[: len(np.unique(y))])
+    def _plot_decision_regions(X, y, classifier, resolution=0.02):
+        colors = ("red", "blue", "lightgreen", "gray", "cyan")
+        colormap = ListedColormap(colors[: len(np.unique(y))])
 
-    X = X.values # as a array
-    x1 = X[:, 0] 
-    x2 = X[:, 1]
-    x1_min, x1_max = x1.min() -1 , x1.max() + 1
-    x2_min, x2_max = x2.min() -1 , x2.max() + 1  
+        X = X.values  # as a array
+        x1 = X[:, 0]
+        x2 = X[:, 1]
+        x1_min, x1_max = x1.min() - 1, x1.max() + 1
+        x2_min, x2_max = x2.min() - 1, x2.max() + 1
 
-    xx1, xx2 = np.meshgrid(np.arange(x1_min, x1_max, resolution), 
-                           np.arange(x2_min, x2_max, resolution))
-    print(xx1)
-    print(xx1.ravel())
-    Z = classfier.predict(np.array([xx1.ravel(), xx2.ravel()]).T)
-    Z = Z.reshape(xx1.shape)
-    plt.contourf(xx1, xx2, Z, alpha=0.2, cmap=cmap)
-    plt.xlim(xx1.min(), xx1.max())
-    plt.ylim(xx2.min(), xx2.max())
-
-    plt.plot()
-
-
-
-  X, y = prepare_data(df)
-
-  _create_base_plot(df)
-  _plot_decision_regions(X, y, model)
-
-  plot_dir = "plots"
-  os.makedirs(plot_dir, exist_ok=True) # ONLY CREATE IF MODEL_DIR DOESN"T EXISTS
-  plotPath = os.path.join(plot_dir, file_name) # model/filename
-  plt.savefig(plotPath)
+        xx1, xx2 = np.meshgrid(np.arange(x1_min, x1_max, resolution),
+                               np.arange(x2_min, x2_max, resolution))
+        print(xx1)
+        print(xx1.ravel())
+        Z = classifier.predict(np.array([xx1.ravel(), xx2.ravel()]).T)
+        Z = Z.reshape(xx1.shape)
+        plt.contourf(xx1, xx2, Z, alpha=0.2, cmap=colormap)
+        plt.xlim(xx1.min(), xx1.max())
+        plt.ylim(xx2.min(), xx2.max())
+        plt.plot()
+    X, y = prepare_data(df)
+    _create_base_plot(df)
+    _plot_decision_regions(X, y, model)
+    plot_dir = "plots"
+    os.makedirs(plot_dir, exist_ok=True)  # ONLY CREATE IF MODEL_DIR DOES NOT EXISTS
+    plotPath = os.path.join(plot_dir, file_name)  # model/filename
+    plt.savefig(plotPath)
