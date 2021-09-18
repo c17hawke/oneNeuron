cbfd8b93a8065b95c6839506905826a9816baf65
SUNNY BHAVEEN CHANDRA <2016peb5126@mnit.ac.in>
2021-09-18 17:33:11 +0530

    docstring upadated

--- utils/all_utils.py
+++ utils/all_utils.py
@@ -9,12 +9,26 @@ plt.style.use("fivethirtyeight")
 
 
 def prepare_data(df):
+    """it is used to separate the dependent variables and independent features
+
+    Args:
+        df (pd.DataFrame): its the pandas DataFrame to
+
+    Returns:
+        tuple: it returns the tuples of dependent variables and independent variables
+    """
     x = df.drop("y", axis=1)
     y = df["y"]
     return x, y
 
 
 def save_model(model, filename):
+    """This saves the trained model to
+
+    Args:
+        model (python object): trained model to
+        filename (str): path to save the trained model
+    """
     model_dir = "models"
     os.makedirs(model_dir, exist_ok=True)  # ONLY CREATE IF MODEL_DIR DOES NOT EXISTS
     filepath = os.path.join(model_dir, filename)  # model/filename
